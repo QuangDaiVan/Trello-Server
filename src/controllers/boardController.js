dd88bfdcee2da98cd4d39655b0cbe47b76018094
caloc <quangdv2802@gmail.com>
2024-08-28 16:14:47 +0700

    find bug

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -2,7 +2,8 @@ import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log('req.body: ', req.body)
+    // console.log('req.body: ', req.body)
+    // throw new Error('tesr error')
     res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create new board' })
   } catch (error) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
