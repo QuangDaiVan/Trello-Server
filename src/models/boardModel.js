5fb9dee3877dd0f4c1c2676fecec21087ebc3a75
caloc <quangdv2802@gmail.com>
2024-08-29 23:10:04 +0700

    validate data at model schema (forgot checkout branch)

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,30 +1,51 @@
 import Joi from 'joi'
+import { ObjectId } from 'mongodb'
 import { GET_DB } from '~/config/mongodb'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
 const BOARD_COLLECTION_NAME = 'boards'
 const BOARD_COLLECTION_SCHEMA = Joi.object({
   title: Joi.string().required().min(3).max(50).trim().strict(),
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(256).trim().strict(),
-  columnOrderIds: Joi.array().items(Joi.string()).default([]),
+
+  columnOrderIds: Joi.array().items(
+    Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+  ).default([]),
+
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
   updatedAt: Joi.date().timestamp('javascript').default(null),
   _destroy: Joi.boolean().default(false)
 })
 
+const validateBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
+}
+
 const createNew = async (data) => {
   try {
-    const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data)
+    // validData: dữ liệu sau khi kiểm tra
+    const validData = await validateBeforeCreate(data)
+    // console.log('validData: ', validData)
+
+    // lấy data đã kiểm tra từ DB
+    const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(validData)
+
+    // trả data về
     return createdBoard
   } catch (error) {
+
+    // bắt lỗi
     throw new Error(error)
   }
 }
 
 const findOneById = async (id) => {
   try {
+    // console.log(id)
+    // console.log(new ObjectId(id))
     const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
-      _id: id
+      _id: new ObjectId(id)
     })
     return result
   } catch (error) {
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -7,16 +7,17 @@ const createNew = async (reqBody) => {
     // xư lý logic dữ liệu tùy đặc thù dự án
     const newBoard = {
       ...reqBody,
-      sluug: slugify(reqBody.title)
+      slug: slugify(reqBody.title),
+      columnOrderIds: ['test']
     }
 
     // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
     const createdBoard = await boardModel.createNew(newBoard)
-    console.log(createdBoard.insertedId)
+    // console.log(createdBoard.insertedId)
 
     // lấy bản ghi board sau khi gọi (tùy mục đích mà dự án cần có bước này hay không)
     const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
-    console.log(getNewBoard)
+    // console.log(getNewBoard)
 
     // Làm thêm các xử lý logic khác với các Collection khác tùy đặc thù dự án
     // Bắn email, notification về cho admin khi có 1 cái board mới được tạo
