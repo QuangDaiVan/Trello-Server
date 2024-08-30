ed1876069b2d546c478d116c1951e45a667771d1
caloc <quangdv2802@gmail.com>
2024-08-30 22:03:06 +0700

    less 62: CORS

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 import express from 'express'
 import exitHook from 'async-exit-hook'
+import cors from 'cors'
+import { corsOptions } from './config/cors'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1'
@@ -8,6 +10,9 @@ import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
 
 const START_SERVER = () => {
   const app = express()
+
+  // xử lý CORS
+  app.use(cors(corsOptions))
 
   // enable req.body data
   app.use(express.json())
