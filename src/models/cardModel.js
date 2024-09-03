cf204ac6ba7361f00b4b04dac7b8526a3d9e4306
caloc <quangdv2802@gmail.com>
2024-09-03 23:17:06 +0700

    less 66: 1/3

--- src/models/cardModel.js
+++ src/models/cardModel.js
@@ -1,11 +1,15 @@
 import Joi from 'joi'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
+import { ObjectId } from 'mongodb'
+import { GET_DB } from '~/config/mongodb'
+
 
 // Define Collection (name & schema)
 const CARD_COLLECTION_NAME = 'cards'
 const CARD_COLLECTION_SCHEMA = Joi.object({
   boardId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
   columnId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
+  slug: Joi.string().required().min(3).trim().strict(),
 
   title: Joi.string().required().min(3).max(50).trim().strict(),
   description: Joi.string().optional(),
@@ -15,7 +19,41 @@ const CARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false)
 })
 
+const validateBeforeCreate = async (data) => {
+  return await CARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
+}
+
+const createNew = async (data) => {
+  try {
+    const validData = await validateBeforeCreate(data)
+
+    // lấy data đã kiểm tra từ DB
+    const createdBoard = await GET_DB().collection(CARD_COLLECTION_NAME).insertOne(validData)
+
+    // trả data về
+    return createdBoard
+  } catch (error) {
+
+    // bắt lỗi
+    throw new Error(error)
+  }
+}
+
+const findOneById = async (id) => {
+  try {
+    const result = await GET_DB().collection(CARD_COLLECTION_NAME).findOne({
+      _id: new ObjectId(id)
+    })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+
 export const cardModel = {
   CARD_COLLECTION_NAME,
-  CARD_COLLECTION_SCHEMA
+  CARD_COLLECTION_SCHEMA,
+  createNew,
+  findOneById
 }
