e3ea8479ad200b78bb86e708839748e0442606b2
caloc <quangdv2802@gmail.com>
2024-09-05 10:00:34 +0700

    less 70: finished

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -22,6 +22,9 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false)
 })
 
+// chỉ định ra những Fields mà không muốn cho phép cập nhật trong hàm update()
+const INVALID_UPDATE_FIELDS = ['_id', 'createdAt']
+
 const validateBeforeCreate = async (data) => {
   return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
 }
@@ -97,10 +100,29 @@ const pushColumnOrderIds = async (column) => {
       { $push: { columnOrderIds: new ObjectId(column._id) } },
       { ReturnDocument: 'after' }
     )
-    return result.value
+    return result
   } catch (error) {
     throw new Error(error)
   }
 }
 
-export const boardModel = { BOARD_COLLECTION_NAME, BOARD_COLLECTION_SCHEMA, createNew, findOneById, getDetails, pushColumnOrderIds }
+const update = async (boardId, updateData) => {
+  try {
+    // Lọc những field mà chúng ta không cho phép cập nhật linh tinh
+    Object.keys(updateData).forEach(fieldName => {
+      if (INVALID_UPDATE_FIELDS.includes(fieldName)) {
+        delete updateData[fieldName]
+      }
+    })
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOneAndUpdate(
+      { _id: new ObjectId(boardId) },
+      { $set: updateData },
+      { ReturnDocument: 'after' }
+    )
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export const boardModel = { BOARD_COLLECTION_NAME, BOARD_COLLECTION_SCHEMA, createNew, findOneById, getDetails, pushColumnOrderIds, update }
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -3,6 +3,7 @@ import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
 import { BOARD_TYPES } from '~/utils/constants'
+// import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
 const createNew = async (req, res, next) => {
   // mặc định phía BE ko cần phải custom message vì trước đó phía FE đã tự validate và custom message phía FE cho đẹp
@@ -43,4 +44,32 @@ const createNew = async (req, res, next) => {
   }
 
 }
-export const boardValidation = { createNew }
+
+const update = async (req, res, next) => {
+  // mặc định phía BE ko cần phải custom message vì trước đó phía FE đã tự validate và custom message phía FE cho đẹp
+  // BE chỉ cần validate đảm bảo dữ liệu chuẩn xác và trả về message mặc định từ thư viện là được
+  // quan trọng: việc validate dư liệu là bắt buộc phải có ở phía BE vì đây là điểm cuối để lưu trữ dữ liệu và Database
+  // và trong thực tế thì điều tốt nhất cho hệ thống là luôn validate dữ liệu ở cả FE và BE
+
+  const correctCondition = Joi.object({
+    // lưu ý: không dùng required trong trường hợp update
+    title: Joi.string().min(3).max(50).trim().strict(),
+    description: Joi.string().min(3).max(256).trim().strict(),
+    type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE)
+    // columnOrderIds: Joi.array().items(
+    //   Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+    // )
+  })
+
+  try {
+
+    // chỉ định thêm abortEarly:false - để hiển thị ra tất cả lỗi
+    // đối với trường hợp update, cho phép unknown để không cần đẩy 1 số field lên
+    await correctCondition.validateAsync(req.body, { abortEarly: false, allowUnknown: true })
+    next()
+  } catch (error) {
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
+  }
+
+}
+export const boardValidation = { createNew, update }
