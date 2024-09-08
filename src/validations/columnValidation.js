986e1d664762c49a4f52b5ceafcc962c9ad590a9
caloc <quangdv2802@gmail.com>
2024-09-08 11:00:45 +0700

    some bugs

--- src/validations/columnValidation.js
+++ src/validations/columnValidation.js
@@ -46,7 +46,7 @@ const update = async (req, res, next) => {
 
 const deleteItem = async (req, res, next) => {
   const correctCondition = Joi.object({
-    id: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
+    id: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
   })
 
   try {
