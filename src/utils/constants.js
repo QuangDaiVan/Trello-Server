dbf0f1815faae05f7aca5367e577cadc1962b19c
caloc <quangdv2802@gmail.com>
2024-11-15 22:43:08 +0700

    add domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,9 +2,9 @@ import { env } from '~/config/environment'
 
 // Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
-  // 'http://localhost:5173' // Không cần localhost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
+  'http://localhost:5173'
 
-  'https://teamwork-trello-be.onrender.com'
+  // 'https://teamwork-trello-be.onrender.com'
 ]
 
 export const BOARD_TYPES = {
