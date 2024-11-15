d3cd9ddad95ba4d0da7f73686323705b0436b61f
caloc <quangdv2802@gmail.com>
2024-11-15 22:30:32 +0700

    fix CORs

--- src/config/cors.js
+++ src/config/cors.js
@@ -7,7 +7,7 @@ import ApiError from '~/utils/ApiError'
 export const corsOptions = {
   origin: function (origin, callback) {
     // Nếu môi trường là local dev thì cho qua luôn
-    if (env.BUILD_MODE === 'dev') {
+    if (!origin && env.BUILD_MODE === 'dev') {
       return callback(null, true)
     }
 
